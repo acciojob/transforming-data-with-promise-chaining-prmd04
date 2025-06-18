1a4a0a8aa762495b82094c20b11e7b7ea9fac9a3
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-18 18:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+let button = document.getElementById("btn");
+let output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+  let number = document.getElementById("ip").value;
+  let firstPromise = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        output.innerText = `Result: ${number}`;
+        resolve(number);
+      }, 2000);
+    });
+  };
+
+  firstPromise()
+    .then((number) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          let result = number * 2;
+          output.innerText = `Result: ${result}`;
+          resolve(result);
+        }, 3000);
+      });
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          let res = result - 3;
+          output.innerText = `Result: ${res}`;
+          resolve(res);
+        }, 4000);
+      });
+    })
+    .then((res) => {
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                let result = res / 2;
+                output.innerText = `Result: ${result}`;
+                resolve(result);
+            },5000)
+        })
+    })
+    .then((result) =>{
+        setTimeout(()=>{
+            let res= result + 10;
+            output.innerText = `Final Result: ${res}`;
+        },6000)
+    })
+});
